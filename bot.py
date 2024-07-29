46d49f77a9d4fc33f6b7a711b69a8fb54954c6f7
BOoblover0 <weblogin4333@gmail.com>
2024-07-29 17:28:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from pyrogram import Client, enums, filters
 from pyrogram.types import BotCommand, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import FloodWait, RPCError
 
-from config import config
+from config import Config
 
 banbot = Client(
         "banbot",
